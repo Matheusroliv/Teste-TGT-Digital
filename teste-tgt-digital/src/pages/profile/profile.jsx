685da3e0677a7f13f01b0ff239d346282e9e080a
Matheus <matheusrdeoliv1@gmail.com>
2021-09-09 09:56:55 -0300

    fazendo mock de dados

--- teste-tgt-digital/src/pages/profile/profile.jsx
+++ teste-tgt-digital/src/pages/profile/profile.jsx
@@ -1,17 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import { useHistory } from "react-router-dom"
 
 export function Profile() {
+  const [post, setPost] = useState(
+    [
+      {
+        title: 'Meu novo site!',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        author: 'mario',
+        id: 1
+      },
+      {
+        title: 'Bem vindos a festa dev',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        author: 'yoshi',
+        id: 2
+      },
+      {
+        title: 'Dicas para Desenvolvedores Web',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...',
+        author: 'mario',
+        id: 3
+      }
+    ])
+
   const history = useHistory()
   const name = window.localStorage.getItem('name')
 
-  const switchPageHandle = (idPage) => {
-    history.push(`${idPage}`)
-  }
-
   return (
     <div>
-      <h1>Bem vindo, {name}</h1>
+      <h1>Bem vindo ao seu Blog, {name}</h1>
+      <div>
+        {post.map((posts) => {
+          return (
+            <div key={posts.id}>
+              <h3>{posts.title}</h3>
+              <p>{posts.content}</p>
+              <p>Escrito por {posts.author}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
